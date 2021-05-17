9e4b5b6450a548240c90635020ecadebec17c530
KayakerS6 <81826132+KayakerS6@users.noreply.github.com>
2021-05-17 09:21:31 -0700

    Add files via upload
    TO DO:
    Logo doesn't display properly; need to use a monospace font
    Add buttons
    Add additional screens that can be navigated to with the afore mentioned buttons
    Add text fields

---
+++ MenuSandbox0.0.1.py
@@ -0,0 +1,15 @@
+from tkinter import *
+from tkinter.ttk import *
+
+root = Tk()
+root.title("PassPY")
+class tkinter.font.Font(family = Courier)
+label = Label(root, text ='________                     __________  __\n___  __ \_____ _________________  __ \ \/ /\n__  /_/ /  __ `/_  ___/_  ___/_  /_/ /_  / \n_  ____// /_/ /_(__  )_(__  )_  ____/_  /  \n/_/     \__,_/ /____/ /____/ /_/     /_/   \n\n\n\n\n').pack()
+
+
+label2 = Label(root, text = 'THE QUICK BROWN FOX JUMPS OVER THE LAZY DOG \nthe quick brown fox jumps over the lazy dog').pack()
+
+
+
+
+root.mainloop()
